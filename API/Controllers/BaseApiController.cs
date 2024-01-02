e3b6b357a2388465900ccf6c5bcd5875bdb5a6cc
Delboy <Derek.degoey@hotmail.co.uk>
2024-01-02 17:06:13 +0000

    End of section 5

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 import Header from "./Header"
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material"
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -17,11 +19,12 @@ function App() {
   })
 
   const handleThemeChange = () => {
-    setDarkMode(!darkMode) 
+    setDarkMode(!darkMode)
   }
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
